d46fb88277caa2a648acc80763177d59bd3905ee
ShervinDani <rvshervin@gmil.com>
2025-06-16 15:02:42 +0530

    Day 2 Changed

--- crm_frontend/src/app/app.routes.ts
+++ crm_frontend/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { HeaderComponent } from './user/header/header.component';
+import { UserdetailsComponent } from './user/userdetails/userdetails.component';
 
 export const routes: Routes = [
     { path: "customer", component:HeaderComponent,
+        children: [
+            { path: "", component: UserdetailsComponent}
+        ]
     }
 ];
